e5bf1a0de9b3618732392f8881d000f9bf947c54
mdrefathossen <refathossen2001@gmail.com>
2023-05-17 18:19:53 +0600

    mui grid system added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { Grid } from '@mui/material';
 import {Navbar} from './components/Navbar';
 import { Result } from './components/Result';
+import { SliderSelect } from './components/SliderSelect';
+import { Tenure } from './components/Tenure';
 
 function App() {
   return (
     <div className="App">
         {/* <Navbar/> */}
         <Navbar/>
-        <Result/>
+        <Grid container spacing={5}>
+          <Grid item>
+            <SliderSelect/>
+            <Tenure/>
+          </Grid>
+          <Grid item>
+            <Result/>
+          </Grid>
+        </Grid>
     </div>
   );
 }
