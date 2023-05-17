5c64600afd7ab05ae1581e3f5a16f509b1661065
mdrefathossen <refathossen2001@gmail.com>
2023-05-17 18:37:24 +0600

    navbar create and design done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <AppBar position="static">
+    <Container maxWidth="xl">
+    <Toolbar>
+      
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Bank Of React
+      </Typography>
+      
+    </Toolbar>
+    </Container>
+  </AppBar>
   )
 }
